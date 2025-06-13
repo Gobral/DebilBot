39a17d31868f977c454061265613ba802bdad58d
Gobral <umpalump97@gmail.com>
2025-06-14 00:49:55 +0200

    Fixed Connection reset by peer with reconnect ffmpeg options

--- debil.py
+++ debil.py
@@ -10,7 +10,7 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
 ytdl_format_options = {
-    "format": "bestaudio/best",
+    "format": "bestaudio/worstvideo",
     "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
     "restrictfilenames": True,
     "noplaylist": True,
@@ -24,6 +24,7 @@ ytdl_format_options = {
 }
 ffmpeg_options = {
     "options": "-vn",
+    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
 }
 
 prefix_return_message = "Przyjąłem"
